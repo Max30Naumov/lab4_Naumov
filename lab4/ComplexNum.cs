f0ed62643c110e372212deecb20b9ca5f6abd02c
Max30Naumov <101650471+Max30Naumov@users.noreply.github.com>
2023-10-02 18:40:52 +0300

    Add project files.

---
+++ lab4/ComplexNum.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class ComplexNum
+    {
+        public double module { get; set; } 
+        public double argument { get; set; }
+        public double realPart { get; set; }
+        public double imaginaryPart { get; set; }
+
+        public ComplexNum()
+        {      
+        }
+        public ComplexNum(double _module, double _argument)
+        {
+            module = _module;
+            argument = _argument;
+            realPart = module * Math.Cos(argument);
+            imaginaryPart = module * Math.Sin(argument);
+        }
+        public double GetModule()
+        {
+            return module;
+        }
+        public double GetArgument()
+        {
+            return argument;
+        }
+
+        public ComplexNum Power(double n)
+        {
+            double newModule = Math.Pow(module, n);
+            double newArgument = argument * n;
+
+            return new ComplexNum(newModule, newArgument);
+        }
+        public static ComplexNum operator +(ComplexNum a, ComplexNum b)
+        {
+            return new ComplexNum
+            {
+                realPart = a.realPart + b.realPart,
+                imaginaryPart = a.imaginaryPart + b.imaginaryPart
+            };
+        }
+
+        public static ComplexNum operator -(ComplexNum a, ComplexNum b)
+        {
+            return new ComplexNum
+            {
+                realPart = a.realPart - b.realPart,
+                imaginaryPart = a.imaginaryPart - b.imaginaryPart
+            };
+        }
+
+        public static ComplexNum operator *(ComplexNum a, ComplexNum b)
+        {
+            return new ComplexNum
+            {
+                realPart = a.realPart * b.realPart - a.imaginaryPart * b.imaginaryPart,
+                imaginaryPart = a.realPart * b.imaginaryPart + a.imaginaryPart * b.realPart
+            };
+        }
+
+        public static ComplexNum operator /(ComplexNum a, ComplexNum b)
+        {
+            double denominator = b.realPart * b.realPart + b.imaginaryPart * b.imaginaryPart;
+            return new ComplexNum
+            {
+                realPart = (a.realPart * b.realPart + a.imaginaryPart * b.imaginaryPart) / denominator,
+                imaginaryPart = (a.imaginaryPart * b.realPart - a.realPart * b.imaginaryPart) / denominator
+            };
+        }
+        public void PrintComplexNumber()
+        {
+            module = Math.Sqrt(realPart * realPart + imaginaryPart * imaginaryPart);
+            argument = Math.Atan2(imaginaryPart, realPart);
+            Console.WriteLine("Комплексное число в тригонометрическом виде:");
+            Console.WriteLine($"R=|{module}|*(Cos({argument})+Sin({argument})i)");
+
+
+            Console.WriteLine("Комплексное число в алгебраическом виде:");
+            Console.WriteLine($"R={realPart}+{imaginaryPart}i");
+        }
+       
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNum a = new ComplexNum(1, 2);
+            ComplexNum b = new ComplexNum(1, 1);
+            ComplexNum c = new ComplexNum(3, 4);
+            ComplexNum d = new ComplexNum(5, 1);
+            ComplexNum R = new ComplexNum();
+           
+            R = b.Power(3) + ((a+b) / (c-a)) *d;
+            R.PrintComplexNumber();
+        }
+    }
+}
